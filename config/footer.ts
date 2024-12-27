982904bde16234289b1065ae88b1bba4221a79b5
javayhu <javayhu@gmail.com>
2024-12-28 01:13:35 +0800

    chore: update links

--- config/footer.ts
+++ config/footer.ts
@@ -13,17 +13,17 @@ const enFooterConfig: SidebarNavItem[] = [
         title: "Features",
         items: [
             { title: "Products", href: "/group/new" },
-            { title: "Indie Apps", href: "/apptype/new" },
-            { title: "Search", external: true, href: "https://link.indiehackers.site/saas101" },
+            { title: "Applications", href: "/apptype/new" },
+            // { title: "Search", external: true, href: "https://link.indiehackers.site/saas101" },
         ],
     },
     {
         title: "Links",
         items: [
             // { title: "Sponsors", external: true, href: "https://buymeacoffee.com/javayhu" },
-            { title: "Mkdirs", external: true, href: "https://mkdirs.com" },
-            { title: "IndieHub", external: true, href: "https://indiehub.best" },
-            { title: "Free OG Generator", external: true, href: "https://og.indiehub.best" },
+            { title: "Mkdirs", external: true, href: "https://mkdirs.com?ref=opensource" },
+            { title: "IndieHub", external: true, href: "https://indiehub.best?ref=opensource" },
+            { title: "Free OG Generator", external: true, href: "https://og.indiehub.best?ref=opensource" },
         ],
     },
 ];
@@ -42,16 +42,19 @@ const zhFooterConfig: SidebarNavItem[] = [
         items: [
             { title: "产品工具", href: "/group/new" },
             { title: "独立应用", href: "/apptype/new" },
-            { title: "搜索工具", external: true, href: "https://link.indiehackers.site/saas101" },
+            // { title: "搜索工具", external: true, href: "https://link.indiehackers.site/saas101" },
         ],
     },
     {
         title: "资源",
         items: [
             // { title: "赞助支持", external: true, href: "https://buymeacoffee.com/javayhu" },
-            { title: "产品规划", external: true, href: "https://indiehackers.canny.io/" },
-            { title: "问题反馈", external: true, href: "https://indiehackers.canny.io/feedback" },
-            { title: "更新日志", external: true, href: "https://indiehackers.canny.io/changelog" },
+            // { title: "产品规划", external: true, href: "https://indiehackers.canny.io/" },
+            // { title: "问题反馈", external: true, href: "https://indiehackers.canny.io/feedback" },
+            // { title: "更新日志", external: true, href: "https://indiehackers.canny.io/changelog" },
+            { title: "Mkdirs", external: true, href: "https://mkdirs.com?ref=opensource" },
+            { title: "IndieHub", external: true, href: "https://indiehub.best?ref=opensource" },
+            { title: "Free OG Generator", external: true, href: "https://og.indiehub.best?ref=opensource" },
         ],
     },
 ];
